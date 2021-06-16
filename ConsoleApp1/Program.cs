b91f84a606fa170ec7c9bf5ea28118ba3f4a895f
oven425 <oven425@yahoo.com.tw>
2021-06-16 18:11:07 +0800

    update

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using QSoft.XmlserializerEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlserializerEx xmlex = new XmlserializerEx();
+            string str = xmlex.Serialize(new
+            {
+                Account="AA",
+                Password="BB",
+                Age=18,
+                Config = new
+                {
+                    Number = 100,
+                    Max =10,
+                    Min=-10
+                }
+            });
+            Console.WriteLine(str);
+            int sum = 0;
+            for(int i=1; i<=3; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine(sum);
+            sum = Sum(3);
+            sum = Sum(1, 3);
+            sum = Sum(1, 5, (x)=>x+2);
+            var vv = Test();
+        }
+
+        static (string, int)Test()
+        {
+            return ("11", 22);
+        }
+
+        static int Sum(int begin, int end, Func<int,int> func)
+        {
+            if (end < begin)
+            {
+                return 0;
+            }
+            return begin + Sum(func(begin), end, func);
+        }
+
+        static int Sum(int begin, int end)
+        {
+            if (end < begin)
+            {
+                return 0;
+            }
+            return begin + Sum(begin + 1, end);
+        }
+
+        static int Sum(int data)
+        {
+            if(data <=0)
+            {
+                return 0;
+            }
+            return data+ Sum(data-1);
+        }
+        
+    }
+}
---
+++ QSoft.XmlserializerEx/XmlserializerEx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace QSoft.XmlserializerEx
+{
+    public class XmlserializerEx
+    {
+        public string Serialize(object src)
+        {
+
+            string result = "";
+            System.Web.Script.Serialization.JavaScriptSerializer json = new System.Web.Script.Serialization.JavaScriptSerializer();
+            string sss = json.Serialize(src);
+
+
+            Type type = src.GetType();
+            XmlWriterSettings setting = new XmlWriterSettings();
+            setting.Indent = true;
+            
+
+            StringBuilder strb = new StringBuilder();
+            XmlWriter xmlWriter = XmlWriter.Create(strb, setting);
+
+            xmlWriter.WriteStartDocument(false);
+            xmlWriter.WriteStartElement("Anonuymous");
+            Serialize(src, xmlWriter);
+
+            xmlWriter.WriteEndElement();
+            xmlWriter.WriteEndDocument();
+            xmlWriter.Close();
+
+            string xml = strb.ToString();
+            Console.WriteLine(xml);
+            return result;
+        }
+
+        void Serialize(object src, XmlWriter xmlw)
+        {
+            var pps = src.GetType().GetProperties();
+            foreach (var pp in pps)
+            {
+                var code = Type.GetTypeCode(pp.PropertyType);
+                switch (Type.GetTypeCode(pp.PropertyType))
+                {
+                    case TypeCode.Boolean:
+                    case TypeCode.SByte:
+                    case TypeCode.Byte:
+                    case TypeCode.Char:
+                    case TypeCode.Single:
+                    case TypeCode.Int16:
+                    case TypeCode.Int32:
+                    case TypeCode.Int64:
+                    case TypeCode.UInt16:
+                    case TypeCode.UInt32:
+                    case TypeCode.UInt64:
+                    case TypeCode.Double:
+                    case TypeCode.Decimal:
+                    case TypeCode.String:
+                        {
+                            string vv = Convert.ToString(pp.GetValue(src));
+                            xmlw.WriteElementString(pp.Name, vv);
+                        }
+                        break;
+                    case TypeCode.Object:
+                        {
+                            xmlw.WriteStartElement(pp.Name);
+                            Serialize(pp.GetValue(src), xmlw);
+                            xmlw.WriteEndElement();
+                        }
+                        break;
+                }
+
+            }
+        }
+    }
+}
