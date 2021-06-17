5ab0f421434bebef4aef8286796345e78d5f5c58
oven425 <oven425@yahoo.com.tw>
2021-06-17 17:56:24 +0800

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,11 @@
 ﻿using QSoft.XmlserializerEx;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ConsoleApp1
 {
@@ -11,20 +13,39 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            
             XmlserializerEx xmlex = new XmlserializerEx();
             string str = xmlex.Serialize(new
             {
-                Account="AA",
+                Array = new int[] { 1,2,3,4,5,6},
+                List = new List<string> { "AA", "BB", "CC" },
+                Account ="AA",
                 Password="BB",
                 Age=18,
+                Time= DateTime.Now,
                 Config = new
                 {
                     Number = 100,
                     Max =10,
                     Min=-10
                 }
-            });
+            }, "AA");
             Console.WriteLine(str);
+
+            XmlSerializer xml = new XmlSerializer(typeof(AA));
+            using (MemoryStream mm = new MemoryStream())
+            {
+                xml.Serialize(mm, new AA());
+                Console.WriteLine(Encoding.UTF8.GetString(mm.ToArray()));
+
+            }
+
+            using (MemoryStream mm = new MemoryStream(Encoding.UTF8.GetBytes(str)))
+            {
+                object oo = xml.Deserialize(mm);
+
+            }
+
             int sum = 0;
             for(int i=1; i<=3; i++)
             {
@@ -36,6 +57,8 @@ namespace ConsoleApp1
             sum = Sum(1, 5, (x)=>x+2);
             var vv = Test();
         }
+
+        
 
         static (string, int)Test()
         {
@@ -69,5 +92,24 @@ namespace ConsoleApp1
             return data+ Sum(data-1);
         }
         
+    }
+
+    public class AA
+    {
+        //public Array Array { set; get; } = new int[] { 1, 2, 3, 4, 5, 6 };
+        public List<string> List { set; get; } = new List<string> { "AA", "BB", "CC" };
+        public string Account { set; get; } = "AA";
+        public string Password { set; get; } = "BB";
+        public int Age { set; get; } = 18;
+        public DateTime Time { set; get; } = DateTime.Now;
+        public Config Config { set; get; }
+    }
+
+    public class Config
+    {
+        [XmlAttribute("Number")]
+        public int Number { set; get; } = 100;
+        public int Max { set; get; } = 10;
+        public int Min { set; get; } = -10;
     }
 }
--- QSoft.XmlserializerEx/XmlserializerEx.cs
+++ QSoft.XmlserializerEx/XmlserializerEx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace QSoft.XmlserializerEx
 {
     public class XmlserializerEx
     {
-        public string Serialize(object src)
+        public string Serialize(object src, string rootname="")
         {
 
             string result = "";
@@ -22,27 +23,67 @@ namespace QSoft.XmlserializerEx
             Type type = src.GetType();
             XmlWriterSettings setting = new XmlWriterSettings();
             setting.Indent = true;
-            
+            setting.Encoding = Encoding.UTF8;
 
-            StringBuilder strb = new StringBuilder();
-            XmlWriter xmlWriter = XmlWriter.Create(strb, setting);
+            //StringBuilder strb = new StringBuilder();
+            MemoryStream mm = new MemoryStream();
+            XmlWriter xmlWriter = XmlWriter.Create(mm, setting);
 
             xmlWriter.WriteStartDocument(false);
-            xmlWriter.WriteStartElement("Anonuymous");
+            xmlWriter.WriteStartElement(string.IsNullOrWhiteSpace(rootname) == true?"Anonuymous": rootname);
             Serialize(src, xmlWriter);
 
             xmlWriter.WriteEndElement();
             xmlWriter.WriteEndDocument();
             xmlWriter.Close();
 
-            string xml = strb.ToString();
-            Console.WriteLine(xml);
+            //result = strb.ToString();
+            result = Encoding.UTF8.GetString(mm.ToArray());
+            mm.Close();
+            mm.Dispose();
             return result;
         }
 
         void Serialize(object src, XmlWriter xmlw)
         {
+            Type src_type = src.GetType();
             var pps = src.GetType().GetProperties();
+            if(pps.Length == 0 || Type.GetTypeCode(src_type) == TypeCode.String)
+            {
+                switch (Type.GetTypeCode(src_type))
+                {
+                    case TypeCode.Boolean:
+                    case TypeCode.SByte:
+                    case TypeCode.Byte:
+                    case TypeCode.Char:
+                    case TypeCode.Single:
+                    case TypeCode.Int16:
+                    case TypeCode.Int32:
+                    case TypeCode.Int64:
+                    case TypeCode.UInt16:
+                    case TypeCode.UInt32:
+                    case TypeCode.UInt64:
+                    case TypeCode.Double:
+                    case TypeCode.Decimal:
+                        {
+                            string vv = Convert.ToString(src);
+                            xmlw.WriteElementString(src_type.Name, vv);
+                        }
+                        break;
+                    case TypeCode.DateTime:
+                        {
+                            DateTime vv = (DateTime)src;
+                            xmlw.WriteElementString(src_type.Name, vv.ToString("o"));
+                        }
+                        break;
+                    case TypeCode.String:
+                        {
+                            xmlw.WriteElementString(src_type.Name, src as string);
+                        }
+                        break;
+                }
+                return;
+            }
             foreach (var pp in pps)
             {
                 var code = Type.GetTypeCode(pp.PropertyType);
@@ -61,17 +102,41 @@ namespace QSoft.XmlserializerEx
                     case TypeCode.UInt64:
                     case TypeCode.Double:
                     case TypeCode.Decimal:
-                    case TypeCode.String:
                         {
                             string vv = Convert.ToString(pp.GetValue(src));
                             xmlw.WriteElementString(pp.Name, vv);
                         }
                         break;
+                    case TypeCode.DateTime:
+                        {
+                            DateTime vv = (DateTime)pp.GetValue(src);
+                            xmlw.WriteElementString(pp.Name, vv.ToString("o"));
+                        }
+                        break;
+                    case TypeCode.String:
+                        {
+                            xmlw.WriteElementString(pp.Name, pp.GetValue(src) as string);
+                        }
+                        break;
                     case TypeCode.Object:
                         {
-                            xmlw.WriteStartElement(pp.Name);
-                            Serialize(pp.GetValue(src), xmlw);
-                            xmlw.WriteEndElement();
+                            var ll = pp.GetValue(src);
+                            if (ll is IEnumerable)
+                            {
+                                xmlw.WriteStartElement(pp.Name);
+                                foreach (var listitem in ll as IEnumerable)
+                                {
+                                    Serialize(listitem, xmlw);
+                                }
+                                xmlw.WriteEndElement();
+                            }
+                            else
+                            {
+                                xmlw.WriteStartElement(pp.Name);
+                                Serialize(pp.GetValue(src), xmlw);
+                                xmlw.WriteEndElement();
+                            }
+                            
                         }
                         break;
                 }
